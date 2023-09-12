e578eacaa5dc4449888f4af791588f5d34ba0bd9
Roman Gutierrez <124226629+Romangutierrez@users.noreply.github.com>
2023-09-12 20:49:17 +0800

    stud-RomanP.Gutierrez.py

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,9 @@
 # Write a function that calculates the area of a circle by a given radius.
-def calculate_circle_area(_):
-    pass
+def areaOfCircle(rad):
+    ar = 3*3.14*rad*rad
+    return ar
+
+print("Enter the Radius of Circle: ", end="")
+r = float(input())
+a = areaOfCircle(r)
+print("\nArea = ", a)
